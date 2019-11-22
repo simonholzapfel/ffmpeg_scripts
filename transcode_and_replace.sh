7d4db1dadf93c5f6793c874799b691967e7fef23
Simon Holzapfel <simon.holzapfel@gmx.at>
2019-11-22 20:21:45 +0100

    fix

--- transcode_and_replace.sh
+++ transcode_and_replace.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for filename in *.mkv; do
-    ffmpeg -i '$filename' -map 0 -c copy -c:v libx265 -crf 18 out.mkv
-    rm '$filename'
-    mv out.mkv '$filename'
+    ffmpeg -i "$filename" -map 0 -c copy -c:v libx265 -crf 18 out.mkv
+    rm "$filename"
+    mv out.mkv "$filename"
 done
