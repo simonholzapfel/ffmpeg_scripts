9c029eabde85ff4e52caa01d2a7d7c76387d9ae6
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-03-09 12:29:59 +0100

    Fixed wildfly deploy template

--- docker/wildfly/wildfly-deploy.ps1
+++ docker/wildfly/wildfly-deploy.ps1
@@ -1,7 +1,7 @@
 $containerName=$args[0]
 
 mvn clean package
-docker build --tag=$containerName .
+docker build --build-arg arg=$containerName --tag=$containerName .
 docker kill $containerName
 docker rm $containerName
 docker run -d -p 8080:8080 --name $containerName $containerName
