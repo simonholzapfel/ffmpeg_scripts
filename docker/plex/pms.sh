9f23b56d754942349f6665bf8946d9977ba62aa5
simonholzapfel <simon.holzapfel@gmx.at>
2020-03-17 21:51:32 +0100

    wip pms.sh

--- docker/plex/pms.sh
+++ docker/plex/pms.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-sudo rm -rf /plxData
+#  Get a claim token at
+#  https://www.plex.tv/claim
+
+# id 'whoami' to get UID and GID, used for drive access rights
+
+# Examples:
+# 1 media drive: /mnt/drive
+# 2 claim token: claim-...
+# 3 UID: 1001
+# 4 GID: 1001
+
 sudo mkdir /plxData
 sudo chmod 777 /plxData
+sudo chmod 777 /plxData/*
 mkdir /plxData/db
 mkdir /plxData/transcode
-docker run -d --name plex-ms --network=host -e TZ="MEZ" -e  PLEX_CLAIM="$2" -v /plxData/db:/config -v /plxData/transcode:/transcode -v "$1":/data plexinc/pms-docker
+docker run -d --name plex-ms --network=host -e PLEX_CLAIM="$2" -e PLEX_UID="$3" -e PLEX_GID="$4" -v /plxData/db:/config -v /plxData/transcode:/transcode -v "$1":/data plexinc/pms-docker
