71dba382f3538b7d6888a55a47d140e4b537e30c
simonholzapfel <simon.holzapfel@gmx.at>
2020-03-22 11:44:00 +0100

    add minecraft version to script

--- docker/minecraft/minecraft-simple.sh
+++ docker/minecraft/minecraft-simple.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# $1 is minecraft version
+
 sudo mkdir /minecraft
 sudo chmod 777 /minecraft
-docker run -d -it -e EULA=TRUE -p 25565:25565 -v /minecraft:/data --name mc itzg/minecraft-server
+docker run -d -it -e EULA=TRUE -e VERSION=$1 -p 25565:25565 -v /minecraft:/data --name mc itzg/minecraft-server
