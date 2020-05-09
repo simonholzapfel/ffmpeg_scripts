16803e6672e93cdeecee0a2c096988ddf0f38386
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-05-09 11:11:04 +0200

    made minecraft-server script scalable

--- docker/minecraft/minecraft-simple.sh
+++ docker/minecraft/minecraft-simple.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # $1 is minecraft version
+# $2 is volume/server id (for scaling)
 
-sudo mkdir /minecraft
-sudo chmod 777 /minecraft
-docker run -d -it -e EULA=TRUE -e VERSION=$1 -p 25565:25565 -v /minecraft:/data --name mc itzg/minecraft-server
+docker run -d -e EULA=TRUE -e VERSION=$1 -p 25565:25565 -v minecraft$2:/data --name minecraft$2 itzg/minecraft-server
