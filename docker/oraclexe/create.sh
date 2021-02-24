4a276d94252861d10bdbd395d22ba861c0d5eceb
Simon Holzapfel <38250977+simonholzapfel@users.noreply.github.com>
2021-02-24 11:47:15 +0100

    Update create.sh

--- docker/oraclexe/create.sh
+++ docker/oraclexe/create.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # $1: database password
 
-docker run --name OracleXE -shm-size=1g -p 1521:1521 -e ORACLE_PWD="$1" -v oracle-vol:/u01/app/oracle/oradata:Z simonholzapfel/oraclexe
+docker run --name OracleXE --shm-size=1g -p 1521:1521 -e ORACLE_PWD="$1" -v oracle-vol:/u01/app/oracle/oradata:Z simonholzapfel/oraclexe
